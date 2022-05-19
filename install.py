10cbb2639143ba6735ab3ddb9b43664c62b111f9
JR <jrangel93@pm.me>
2022-05-19 14:12:47 -0500

    feat(py): add install in python

---
+++ install.py
@@ -0,0 +1,22 @@
+import os
+
+name_bench = input('Name for bench init: ')
+site_name = input('Name for site: ')
+db_name = input('Name for Database: ')
+
+os.system(f"bench init {name_bench or 'frappe-bench'} --frappe-branch version-13\
+	&& cd frappe-bench &&\
+	bench get-app https://git.gonext.com.mx/frappe/frappe.git --branch prod &&\
+	rm -rf apps/frappe/frappe/integrations/doctype/twilio_settings &&\
+	bench get-app https://git.gonext.com.mx/frappe/erpnext.git --branch prod &&\
+	bench get-app https://git.gonext.com.mx/ibravo/Mobile.git --branch dev &&\
+	bench get-app https://git.gonext.com.mx/valsa/e_billing.git --branch prod &&\
+	bench get-app https://git.gonext.com.mx/valsa/sales_drive.git --branch prod &&\
+	bench get-app https://git.gonext.com.mx/valsa/edi.git --branch prod &&\
+	bench get-app https://git.gonext.com.mx/valsa/cheque_management.git --branch prod &&\
+	bench get-app https://git.gonext.com.mx/valsa/valsa.git --branch prod &&\
+	bench get-app https://git.gonext.com.mx/valsa/logistics.git --branch prod &&\
+	bench new-site {site_name} --db-name '{db_name}' &&\
+	touch sites/currentsite.txt && echo {site_name} > sites/currentsite.txt &&\
+	bench --site valsa.site set-config developer_mode true &&\
+	bench --site valsa.site install-app frappe erpnext mobile e_billing sales_drive edi cheque_management logistics valsa")
